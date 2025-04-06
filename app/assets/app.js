1ff364aff4f83423ff839e3156a132ce82d4ea32
Simon McWhinnie <simon.mcwhinnie@immediate.co.uk>
2025-04-06 15:28:28 +0100

    fix for mobile, add checkbox on clicking step

--- app/assets/app.js
+++ app/assets/app.js
@@ -8,5 +8,13 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import './styles/global.scss';
+
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
+
+
+document.querySelectorAll('.click-to-complete').forEach(el => {
+    el.addEventListener('click', () => {
+        el.classList.toggle('done');
+    });
+});
